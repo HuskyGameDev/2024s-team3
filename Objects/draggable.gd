945660d9ebe584900b6f29c546b90e9a001b57ec
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-01-26 15:51:00 -0500

    simplify drag and drop code

--- Objects/draggable.gd
+++ Objects/draggable.gd
@@ -1,39 +1,20 @@
 extends RigidBody2D
 
-@export var min_impulse_catch : Vector2 = Vector2(-10,-10)
-@export var max_impulse_catch : Vector2 = Vector2 (10,10)
-signal clicked
-
-var held = false
+var beingHeld = false
 
 func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			print("clicked")
-			clicked.emit(self)
+	if Input.is_action_just_pressed("click"):
+		beingHeld = true
+		freeze = true
+	elif Input.is_action_just_released("click"):
+		beingHeld = false
+		freeze = false
+		self.apply_central_impulse((Input.get_last_mouse_velocity()/2))
 
 
 func _physics_process(delta):
-	if held:
+	if beingHeld:
 		global_transform.origin = get_global_mouse_position()
 
 
-func pickup():
-	if held:
-		return
-	freeze = true
-	held = true
-
-
-func drop(impulse):
-	if held:
-		freeze = false
-		print(impulse)
-		apply_central_impulse(impulse)
-		held = false
-
-
-
 ##set_collision_mask_value(1,false)
-
-
