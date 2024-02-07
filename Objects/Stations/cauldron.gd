e1dc0ad97fd8f25c11358223a28ba6be155559ea
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-02-06 21:34:35 -0500

    move ingredient to top of cauldron

--- Objects/Stations/cauldron.gd
+++ Objects/Stations/cauldron.gd
@@ -12,6 +12,14 @@ func _on_recipes_recipes_loaded(recipe_dict):
 
 func _on_body_enter_cauldron(body):
 	if body.get("object_type") == "Ingredient":
+		## Move object to top of cauldron
+		var top_of_cauldron = self.position - Vector2(0, 80)
+		var ingredient_position = get_global_mouse_position()
+		var time_to_animate = (top_of_cauldron.distance_to(ingredient_position)) / 1000
+		var tween = create_tween().bind_node(body)
+		tween.tween_property(body, "global_position", top_of_cauldron, time_to_animate).from(ingredient_position)
+		await tween.finished
+		## Add to cauldron array
 		var addedIngredient = body.get("object_data")
 		body.queue_free()
 		currentIngredients.push_back(addedIngredient.id)
