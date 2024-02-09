d82b0dfab26d800cebffdf4527b5b76615b1b00b
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-02-09 13:16:36 -0500

    update drag and drop to use forces

--- Objects/draggable.gd
+++ Objects/draggable.gd
@@ -3,25 +3,31 @@ extends RigidBody2D
 ############ Drag and Drop Functions ############
 var beingHeld = false
 
+
+func _unhandled_input(event):
+	if not Input.is_action_pressed("click") and beingHeld:
+		beingHeld = false
+		set_deferred("gravity_scale", 1)
+		set_collision_layer(1)
+
+
 func _on_input_event(_viewport, _event, _shape_idx):
 	if Input.is_action_just_pressed("click"):
 		beingHeld = true
-		set_deferred("freeze", true)
+		set_deferred("gravity_scale", 0)
 		set_collision_layer(32)
 		var tween = create_tween()
 		tween.tween_property(self, "rotation", 0, 0.1)
-	elif Input.is_action_just_released("click"):
-		beingHeld = false
-		set_deferred("freeze", false)
-		set_collision_layer(1)
-		self.apply_central_impulse((Input.get_last_mouse_velocity()/2))
-
-
-func _physics_process(_delta):
+	
+	
+func _integrate_forces(state):
 	if beingHeld:
-		#global_transform.origin += get_local_mouse_position()
-		global_transform.origin = get_global_mouse_position()
-		
+		var mousePos = get_global_mouse_position()
+		var distance = global_position.distance_to(mousePos)
+		var direction = global_position.direction_to(mousePos)
+		rotation = 0 
+		set_linear_velocity(direction * distance * 15)
+
 
 ############### Object Functions ################
 @export_enum("Potion", "Ingredient") var object_type: String
