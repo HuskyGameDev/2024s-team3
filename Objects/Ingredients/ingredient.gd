3eed5e159716a09bcec5f8f92fc99afdfa808898
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-02-09 14:10:20 -0500

    remove unnecessary setup signals

--- Objects/Ingredients/ingredient.gd
+++ Objects/Ingredients/ingredient.gd
@@ -2,7 +2,7 @@ extends Node
 
 @export var data: Resource
 
-func _on_draggable_object_ready():
+func _ready():
 	$"DraggableObject".set("object_type", "Ingredient")
 	$"DraggableObject".set("object_data", data)
 	$"DraggableObject".call("data_updated")
--- Objects/Potions/potion.gd
+++ Objects/Potions/potion.gd
@@ -3,7 +3,7 @@ extends Node
 @export var data: Resource
 var rigidbody: RigidBody2D
 
-func _on_draggable_object_ready():
+func _ready():
 	$"DraggableObject".set("object_type", "Potion")
 	$"DraggableObject".set("object_data", data)
 	$"DraggableObject".call("data_updated")
