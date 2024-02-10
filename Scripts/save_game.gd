b9dfca5a0952d3afb2dfd0112dc00a3cf54edd15
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-02-10 15:40:13 -0500

    don't break if there is no save file

--- Scripts/save_game.gd
+++ Scripts/save_game.gd
@@ -1,36 +1,36 @@
 extends Node
 
 const SAVE_LOCATION = "user://savegame.save"
-
+@export var saveFile:SaveGameFile
 
 func _ready():
-	var save = load_game()
-	#print(save.name)
+	saveFile = load_game()
 	
 
 func save_game():
 	## Set up save object
 	var save = SaveGameFile.new()
-	save.name = "test"
 	
 	## Save to file
 	var save_file = FileAccess.open(SAVE_LOCATION, FileAccess.WRITE)
 	save_file.store_var(var_to_str(save))
 
 
-func load_game():
+func load_game() -> SaveGameFile:
 	## Load from file
 	if not FileAccess.file_exists(SAVE_LOCATION):
 		print("No save found")
-		return
+		return SaveGameFile.new()
 	var save_file = FileAccess.open(SAVE_LOCATION, FileAccess.READ)
 	var save = str_to_var(save_file.get_var())
 	if save is SaveGameFile:
 		return save
 	else:
 		print("error reading save file")
-		return
-	
+		return SaveGameFile.new()
+
 
 func clear_save():
 	DirAccess.remove_absolute(SAVE_LOCATION)
+
+
