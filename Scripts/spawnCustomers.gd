a75342ba53b07a35847a761b834572e6dd9e81aa
Chris Apfel <cdapfel@mtu.edu>
2024-02-14 08:32:20 -0500

    spawnCustomers script movement
    Testing small movement to make sprites feel more lifelike without animating them

--- Scripts/spawnCustomers.gd
+++ Scripts/spawnCustomers.gd
@@ -1,8 +1,13 @@
 extends Node
 
 #var this = preload("res://Scenes/customers/customer1.tscn") # testing, reference the object to instantiate as a scene 
-
+var tempCust
+var stop = false
+var customerStartLocation = (Vector2(500,50))
+var customerEndLocation = (Vector2(450,50))
+#var custspawned = false;
 var sizeOfCustomers = 3
+var t = 0.0
 
 var customer1 = preload("res://Scenes/customers/customer1.tscn")
 var customer2 = preload("res://Scenes/customers/customer2.tscn")
@@ -23,8 +28,6 @@ func _ready():
 	
 	
 	# create a customer array to spawn in unique sprites, order details will be random but seperated from customer (for now, will change) 
-	var customerLocation = (Vector2(400,50))
-	
 	var custArray = [customer1, customer2, customer3] 
 	
 	print("uninstantiated objects in array:")
@@ -34,10 +37,28 @@ func _ready():
 	custArray.shuffle() # will randomize the array
 	
 	# instatiate number 1 test
-	var tempCust = custArray[0].instantiate()
-	tempCust.position = customerLocation
+	tempCust = custArray[0].instantiate()
+	tempCust.position = customerStartLocation
 	add_child(tempCust) # add new custer to the main scene so you can see it
-	
+	#custspawned = true
+
+
+func _physics_process(delta): # testing a small movement without animating it
+	t += delta * 0.4
+	#print(t)
+	tempCust.position = customerStartLocation.lerp(customerEndLocation, t)
+
+
+#func endPhysics():
+	#wait(15)
+	#set_physics_process(false)
+
+
+#func wait(seconds: float) -> void:
+	#await get_tree().create_timer(seconds).timeout
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if t > 2:
+		set_physics_process(false)
