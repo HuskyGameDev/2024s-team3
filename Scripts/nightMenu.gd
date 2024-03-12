12d2149ba6ca3c5be1bb9cd40759a7bfdcb8bb70
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-03-12 19:44:03 -0400

    Update night screen file links

--- Scripts/nightMenu.gd
+++ Scripts/nightMenu.gd
@@ -3,7 +3,7 @@ extends Node
 
 func _on_buy_button_pressed():
 	print("buying ingredients not implemented yet")
-	get_tree().change_scene_to_file("res://Scenes/Levels/Main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Screens/Main.tscn")
 
 
 func _on_forage_button_pressed():
@@ -11,9 +11,9 @@ func _on_forage_button_pressed():
 	print("Foraged items:")
 	for i in foragedItems:
 		print("  -" + i.ingredient_name)
-	get_tree().change_scene_to_file("res://Scenes/Levels/Main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Screens/Main.tscn")
 
 
 func _on_move_button_pressed():
 	print("moving not implemented yet")
-	get_tree().change_scene_to_file("res://Scenes/Levels/Main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Screens/Main.tscn")
