6cc5414bb978f970dfe05e19203cf6096a652a4d
Chris Apfel <cdapfel@mtu.edu>
2024-03-12 05:53:11 -0400

    Customer Signals for Potions
    Check the console to see what a customer wants, and try giving them a potion.

--- Scripts/customer.gd
+++ Scripts/customer.gd
@@ -1,9 +1,11 @@
 extends Node
 # creates generic customers 
 
-var customerName = "joe" # default 
+var potionsRecieved: Array[String] = [] # Add potion recieved to an array for storage, can combine or add to one main array in the future to keep track of all potions
+
+var customerName = "joe" # default, can also look at the acutal object name too
 var walkSpeed = .25 # default
-var foodOrder = ""
+var foodOrder = "(notSet)"
 	
 func setCustomerSpeed(speed):
 	self.walkSpeed = speed # same as "this. in java"
@@ -13,3 +15,20 @@ func setCustomerName(name):
 	
 func setCustomerOrder(order):
 	self.foodOrder = order 
+
+func _on_area_2d_input_event(viewport, event, shape_idx): # Godot signal I might use later
+	#if event is Potion: 
+		#print("customer got a : ", Potion) 
+		pass
+
+func _on_area_2d_body_entered(body):
+	if body.get("object_type") == "Potion":
+		var recievedPotion = body.get("object_data")
+		
+		if (recievedPotion.id == foodOrder): # check if they got the right one
+			print(customerName, "recieved a ", foodOrder)
+		else: print(customerName, " wanted a: ", foodOrder, " but got a " ,recievedPotion.id, " instead.")
+		
+		## Add potion recieved to an array 
+		var addedPotion = body.get("object_data")
+		potionsRecieved.push_back(recievedPotion.id) # add
--- Scripts/spawnCustomers.gd
+++ Scripts/spawnCustomers.gd
@@ -6,12 +6,17 @@ extends Node
 # the customers can be created with different attributes in this script like walking speed, lore, order, ect. or default values 
 # more customers with different looks are made seperatly by creating a scence, the object itself is a scene that will be instatiated when it is needed later to save reasources
 
+var PotionNames = ["healing_potion", "potion2", "potion3", "potion4"] # what they might order
+
 var tempCust
-var customerStartLocation = (Vector2(750,50))
-var customerEndLocation = (Vector2(400,50))
+var customerStartLocation = (Vector2(1200,50))
+var customerEndLocation = (Vector2(800,50)) # changing y value could create a walking bobbing effect
+var customerWalkFlip = true
+
 #var custspawned = false;
 var sizeOfCustomers = 3
-var t = 0.0 # time
+var t = 0.0 # interpolation t walk value for the x direction
+var y = 0.0 # interpolation t up and down walk value for the y direction
 
 var customer1 = preload("res://Scenes/Customers/customer1.tscn")
 var customer2 = preload("res://Scenes/Customers/customer2.tscn")
@@ -38,21 +43,40 @@ func _ready():
 	add_child(tempCust) # add new customer to the main scene so you can see it
 	#custspawned = true
 	
-	var my_random_number = rng.randf_range(.1, 2.5) # walking speed range
+	# generate customer data example
+	#todo 
+	tempCust.setCustomerName("customerOne")
+	tempCust.setCustomerOrder(PotionNames.pick_random()) 
+	
+	# generate customers one after another
+	#todo
+	
+	var my_random_number = rng.randf_range(.1, 1.5) # walking speed range
 	tempCust.setCustomerSpeed(my_random_number) # reference the current instance of a customer and modify it, AFTER they are instatiated 
 	
 	print(tempCust.name, " has entered with a speed of: ", my_random_number, "\n")
+	print(tempCust.name, " wants a ", tempCust.foodOrder)
 	
-func _physics_process(delta): # testing a small movement without animating it
-	t += (delta * (tempCust.walkSpeed * 2) )
-	#print(t) 
-	tempCust.position = customerStartLocation.lerp(customerEndLocation, t)
-
+func _physics_process(delta): # testing a small movement without animating it, godot makes this strange idk
+	# x
+	if(t < 1.5):
+		t += (delta * (tempCust.walkSpeed) )
+		#print(t) 
+		tempCust.position = customerStartLocation.lerp(customerEndLocation, t) 
+	
+	# y 
+	else:
+		customerEndLocation.y += .3
+		t += (delta * (tempCust.walkSpeed) )
+		tempCust.position = customerStartLocation.lerp(customerEndLocation, t) 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (t > 2 && t != 0.0):
+	if (t > 2 || t < -2):
 		set_physics_process(false)
 		t = 0.0
 		
 #func wait(seconds: float) -> void:
 	#await get_tree().create_timer(seconds).timeout
+	
+
