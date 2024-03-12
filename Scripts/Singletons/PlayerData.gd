7861d651651436db9efeb65c6a4ed6c3fcdba9d4
Wade Canavan <112517708+WadeCanavan@users.noreply.github.com>
2024-03-12 17:16:49 -0400

    able to base number of slots in drawer off of JSON FIle

--- Scripts/Singletons/PlayerData.gd
+++ Scripts/Singletons/PlayerData.gd
@@ -2,14 +2,23 @@ extends Node
 
 ################# Game Saving ###################
 const SAVE_LOCATION = "user://savegame.save"
+var INV_LOCATION = "user://inv_data_file.json"
+var data = {}
 @export var save:SaveGameFile
 
 func _ready():
+	create_new_inv()
 	save = load_game()
 	GameTime.connect("end_of_day", _on_end_of_day);
 	
 
-func save_game():
+func save_game(content):	
+	if content != null:
+		var file = FileAccess.open(INV_LOCATION, FileAccess.WRITE)
+		file.store_string(JSON.stringify(content))
+		file.close
+		file = null
+		
 	var save_file = FileAccess.open(SAVE_LOCATION, FileAccess.WRITE)
 	save_file.store_var(var_to_str(save))
 
@@ -27,6 +36,16 @@ func load_game() -> SaveGameFile:
 		print("error reading save file")
 		return SaveGameFile.new()
 
+func read_inv():
+	var file = FileAccess.open(INV_LOCATION, FileAccess.READ)
+	var content = JSON.parse_string(file.get_as_text())
+	return content
+	
+func create_new_inv():
+	var file = FileAccess.open("res://Assets/Data/inv_data_file.json", FileAccess.READ)
+	var content = JSON.parse_string(file.get_as_text())
+	data =  content
+	save_game(content)
 
 func clear_save():
 	DirAccess.remove_absolute(SAVE_LOCATION)
@@ -35,7 +54,7 @@ func clear_save():
 ################ Event Triggers #################
 
 func _on_end_of_day():
-	save_game();
+	save_game(null)
 	
 func _on_potion_made(potion:Potion):
 	if not save.madePotions.has(potion.id):
--- Scripts/drawer.gd
+++ Scripts/drawer.gd
@@ -2,9 +2,22 @@ extends Control
 
 var gridContainer : GridContainer
 var items :Array
+var template_inv_slot = preload("res://Scenes/UI/Drawer_button.tscn")
+@onready var gridcontainer = get_node("Background/M/V/ScrollContainer/GridContainer")
+var inv_data
+var slots = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gridContainer = $Background/M/V/ScrollContainer/GridContainer
+	inv_data = PlayerData.read_inv()
+	for i in inv_data:
+		slots.push_front(i)
+		print(i)
+		#var inv_slot_new = template_inv_slot.instantiate()
+		#if PlayerData.inv_data[i]["Item"] != null:
+			#var item_name = PlayerData.item_data[str(PlayerData.inv_data[i]["Item"])]["Name"]
+			#var icon_texture = load("res://Assets/Resources/Ingredients/" + item_name + ".tres")
+			#inv_slot_new.get_node("Icon").set_texture(icon_texture)
+		#gridcontainer.add_child(inv_slot_new, true)
 	populateButtons()
 	pass # Replace with function body.
 
@@ -14,7 +27,7 @@ func _process(delta):
 	pass
 
 func populateButtons():
-	for i in 20:
+	for i in inv_data:
 		var packedScene = ResourceLoader.load("res://Scenes/UI/Drawer_button.tscn")
 		var itemButton = packedScene.instantiate()
 		itemButton.connect("OnButtonClick", OnButtonClicked)
