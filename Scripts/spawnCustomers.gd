83045318969e9f6a34b3b2f0411540abcec21437
Chris Apfel <cdapfel@mtu.edu>
2024-03-12 06:25:46 -0400

    Took change out of main, hit eye on CustomerSpawner too see customers

--- Scripts/spawnCustomers.gd
+++ Scripts/spawnCustomers.gd
@@ -6,17 +6,17 @@ extends Node
 # the customers can be created with different attributes in this script like walking speed, lore, order, ect. or default values 
 # more customers with different looks are made seperatly by creating a scence, the object itself is a scene that will be instatiated when it is needed later to save reasources
 
-var PotionNames = ["healing_potion", "potion2", "potion3", "potion4"] # what they might order
+var areaOnePotionNames = ["healing_potion", "potion2", "potion3", "potion4"] # what they might order
 
 var tempCust
 var customerStartLocation = (Vector2(1200,50))
 var customerEndLocation = (Vector2(800,50)) # changing y value could create a walking bobbing effect
-var customerWalkFlip = true
+#var customerWalkFlip = true
 
 #var custspawned = false;
 var sizeOfCustomers = 3
 var t = 0.0 # interpolation t walk value for the x direction
-var y = 0.0 # interpolation t up and down walk value for the y direction
+#var y = 0.0 # interpolation t up and down walk value for the y direction
 
 var customer1 = preload("res://Scenes/Customers/customer1.tscn")
 var customer2 = preload("res://Scenes/Customers/customer2.tscn")
@@ -46,12 +46,13 @@ func _ready():
 	# generate customer data example
 	#todo 
 	tempCust.setCustomerName("customerOne")
-	tempCust.setCustomerOrder(PotionNames.pick_random()) 
+	tempCust.setCustomerOrder(areaOnePotionNames.pick_random()) # area specific lists? like the one above
 	
-	# generate customers one after another
+	# generate customers one after another 
+	# how many per area?
 	#todo
 	
-	var my_random_number = rng.randf_range(.1, 1.5) # walking speed range
+	var my_random_number = rng.randf_range(.4, 1.4) # walking speed range
 	tempCust.setCustomerSpeed(my_random_number) # reference the current instance of a customer and modify it, AFTER they are instatiated 
 	
 	print(tempCust.name, " has entered with a speed of: ", my_random_number, "\n")
@@ -59,22 +60,26 @@ func _ready():
 	
 func _physics_process(delta): # testing a small movement without animating it, godot makes this strange idk
 	# x
-	if(t < 1.5):
+	if(t < 1.7):
 		t += (delta * (tempCust.walkSpeed) )
 		#print(t) 
 		tempCust.position = customerStartLocation.lerp(customerEndLocation, t) 
 	
-	# y 
+	# y slow down walk at the end, and move in y direction
 	else:
-		customerEndLocation.y += .3
+		customerEndLocation.y += 1
+		if(tempCust.walkSpeed > 1 ):
+			tempCust.walkSpeed -= .1
+		
 		t += (delta * (tempCust.walkSpeed) )
+		#print(t) 
 		tempCust.position = customerStartLocation.lerp(customerEndLocation, t) 
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (t > 2 || t < -2):
+	if (t >= 2 || t < 0):
 		set_physics_process(false)
-		t = 0.0
+		#t = 0.0
 		
 #func wait(seconds: float) -> void:
 	#await get_tree().create_timer(seconds).timeout
