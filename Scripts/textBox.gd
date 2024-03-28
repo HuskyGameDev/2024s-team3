c0a2b30f28d0162a7a22123639834cd3d28de187
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-03-28 17:28:26 -0400

    update customer textbox theme to match tooltip

--- Scripts/textBox.gd
+++ Scripts/textBox.gd
@@ -3,9 +3,7 @@ extends CanvasLayer
 # const CHAR_READ_RATE = 0.05 # might use this
 
 @onready var textbox_container = $TextboxContainer
-@onready var start_symbol = $TextboxContainer/MarginContainer/HBoxContainer/Start
-@onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
-@onready var label = $TextboxContainer/MarginContainer/HBoxContainer/Label
+@onready var label = $TextboxContainer/Label
 
 @export var order: String
 
@@ -14,13 +12,10 @@ func _ready(): # keep this there is a bug, for now
 	add_text("text to be added!")
 
 func hide_textbox():
-	start_symbol.text = ""
-	end_symbol.text = ""
 	label.text = ""
 	textbox_container.hide() # hides the text only
 	
 func show_textbox():
-	start_symbol.text = "*"
 	textbox_container.show()
 	
 func add_text(next_text):
