a94413103591af33045077d26868f7297c6a11d9
Chris Apfel <cdapfel@mtu.edu>
2024-04-02 10:25:09 -0400

    bell fix
    Clicks correctly now, also I did a quick PS of the Customer Sprites because they were massive rectangles.

--- Scenes/Screens/ringBell.gd
+++ Scenes/Screens/ringBell.gd
@@ -5,13 +5,6 @@ signal CorrectGoToCustSpawner
 var custOrder 
 var potionOnPedestal
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("click"):
-		if (custOrder == potionOnPedestal):
-			print("correct order!")
-			CorrectGoToCustSpawner.emit() # send another one, get ride of this guy 
-			
 
 func _on_pedestal_send_to_bell(item):
 	potionOnPedestal = item.name
@@ -19,3 +12,9 @@ func _on_pedestal_send_to_bell(item):
 
 func _on_customer_spawner_order_to_bell(data:String):
 	custOrder = data
+
+func _pressed():
+	if (custOrder == potionOnPedestal):
+		print("correct order!")
+		CorrectGoToCustSpawner.emit() # send another one, get ride of this guy 
+		potionOnPedestal = null
