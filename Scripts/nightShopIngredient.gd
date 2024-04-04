d551dd0015ee16b17de159f055749cfd888e233d
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-04-04 15:25:50 -0400

    buy ingredient button disabling

--- Scripts/nightShopIngredient.gd
+++ Scripts/nightShopIngredient.gd
@@ -1,16 +1,46 @@
 extends Node
 
 var cost:int
-var quantity:int
+var quantityAvailable:int
 var item
+var quantityBuying:int
+
+var increaseButton:Button
+var decreaseButton:Button
+var quantityLabel:Label
+
+func _ready():
+	increaseButton = $"AmountHBox/IncreaseButton"
+	decreaseButton = $"AmountHBox/DecreaseButton"
+	quantityLabel = $"AmountHBox/QuantityLabel"
+	_updateQuantityButtons()
 
 func setData(offering:Dictionary):
 	self.cost = offering.get("cost")
-	self.quantity = offering.get("quantity")
+	self.quantityAvailable = offering.get("quantity")
 	self.item = offering.get("item")
+	self.quantityBuying = 0
 	_updateLabels()
 	
 func _updateLabels():
 	$"NameLabel".text = item.name + " - $" + str(cost)
-	$"ItemTexture/AmountForSaleLabel".text = str(quantity)
+	$"ItemTexture/AmountForSaleLabel".text = str(quantityAvailable)
 	$"ItemTexture".texture = item.image
+
+
+func _on_decrease_button_pressed():
+	quantityBuying -= 1
+	_updateQuantityButtons()
+
+func _on_increase_button_pressed():
+	quantityBuying += 1
+	_updateQuantityButtons()
+
+func _updateQuantityButtons():
+	quantityLabel.text = str(quantityBuying)
+	
+	if quantityBuying == 0: decreaseButton.disabled = true
+	else: decreaseButton.disabled = false
+		
+	if quantityBuying == quantityAvailable: increaseButton.disabled = true
+	else: increaseButton.disabled = false
