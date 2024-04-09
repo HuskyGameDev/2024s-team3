3faf4b481b2801d224eca5a675a3b3e1111f54a7
Emilie Rummer <114100983+emilierummer@users.noreply.github.com>
2024-04-09 17:38:17 -0400

    don't progress time during main menu

--- Scenes/UI/Title.gd
+++ Scenes/UI/Title.gd
@@ -2,6 +2,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GameTime.stop()
 	pass # Replace with function body.
 
 
--- Scripts/Singletons/GameTime.gd
+++ Scripts/Singletons/GameTime.gd
@@ -2,7 +2,7 @@ extends Timer
 
 const STORE_OPEN_TIME = 9;
 const STORE_CLOSE_TIME = 17;
-const GAME_TIME_SCALE = 60; # The number of irl seconds per 1 in game hour
+const GAME_TIME_SCALE = 1; # The number of irl seconds per 1 in game hour
 
 signal end_of_day;
 signal start_of_day;
@@ -14,7 +14,6 @@ func _ready():
 	self.connect("timeout", _on_timer_timeout);
 	self.connect("end_of_day", _on_end_of_day);
 	self.connect("start_of_day", _on_start_of_day);
-	start_of_day.emit();
 
 func _on_timer_timeout():
 	hour = hour + 1;
@@ -28,3 +27,6 @@ func _on_end_of_day():
 func _on_start_of_day():
 	hour = STORE_OPEN_TIME;
 	self.start(GAME_TIME_SCALE);
+
+func start_day():
+	start_of_day.emit();
