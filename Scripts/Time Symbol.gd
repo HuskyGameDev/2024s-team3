c69c08d9fd4e36e7528532b207dd279d372fc0a4
Ben Kornas <93051981+Beancan7@users.noreply.github.com>
2024-04-10 20:30:10 -0400

    Added Clock to main scene along with some sprite changes

--- Scripts/Time Symbol.gd
+++ Scripts/Time Symbol.gd
@@ -1,7 +1,18 @@
 extends Sprite2D
 
-var rotation_speed = 0.001
+var rotation_speed = 0.0001
+@onready var tween = create_tween()
+
+func _ready():
+	tween.tween_method(_symbol_rotation, 0, 360, (abs(GameTime.STORE_OPEN_TIME - GameTime.STORE_CLOSE_TIME) * GameTime.GAME_TIME_SCALE)).set_trans(Tween.TRANS_LINEAR)
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	global_rotation += (rotation_speed + randf_range(.003, .006))
+#func _process(delta):
+	#global_rotation -= rotation_speed
+
+
+
+func _symbol_rotation(value: int):
+	rotation_degrees = -value
