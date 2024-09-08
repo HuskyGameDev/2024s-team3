55c503232b64669595885313f2b6588805be1dc5
Ben Kornas <93051981+Beancan7@users.noreply.github.com>
2024-09-08 14:42:42 -0400

    Changed Clock and Chain Files

--- scenes/main/ui/clock/Time Circles2.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-#var rotation_speed = 0.001
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#global_rotation += rotation_speed
