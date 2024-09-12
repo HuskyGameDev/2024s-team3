076a045de32036ce51a6e3ecc4ce5a0ae7a32a37
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-09-11 21:40:31 -0400

    feat: make inventory behave as drag and drop slots

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -15,6 +15,8 @@ func _on_shelf_body_entered(body):
 	body.rotation = 0
 
 
+func _process(delta):
+	print(get_viewport().gui_get_hovered_control())
 ###################### POTION SELLING ######################
 var sellingPotion: Potion ## the potion on the pedestal ready to be sold
 
--- scenes/main/shelf_slot/shelf_slot.gd
+++ scenes/main/shelf_slot/shelf_slot.gd
@@ -2,6 +2,7 @@ extends Node
 
 var dragging: DraggableObject #if something is being dragged over the slot
 var heldNode: DraggableObject #the node in the slot
+var nodeShouldCenter: bool = false #should the node be centered
 
 @export var centerItems: bool = true
 @export var allowMultipleItems: bool = false
@@ -11,6 +12,11 @@ signal item_changed(newItem: Item)
 ## To change what kind of item can be put in the slot change the Area2D's mask:
 ## Ingredients: 31
 ## Potions: 32
+
+func _process(delta):
+	if heldNode and not heldNode.beingHeld and nodeShouldCenter \
+	and heldNode.global_position != $SlotCollider.global_position:
+		heldNode.global_position = $SlotCollider.global_position
 
 func _on_slot_hover_entered(body):
 	if body is DraggableObject:
@@ -28,7 +34,7 @@ func _on_slot_hover_exited(body):
 
 
 func _unhandled_input(event):
-	if Input.is_action_just_released("click") && dragging:
+	if Input.is_action_just_released("click") && dragging && not dragging.onShelf:
 		if not allowMultipleItems and heldNode: return
 		heldNode = dragging
 		item_changed.emit(heldNode.data)
@@ -38,7 +44,9 @@ func _unhandled_input(event):
 		heldNode.set_deferred("freeze", true)
 		var tween = create_tween()
 		tween.tween_property(heldNode, "rotation", 0, 0.1)
+		nodeShouldCenter = false
 		if centerItems:
 			tween.tween_property(heldNode, "global_position", $SlotCollider.global_position, 0.1)
 		await tween.finished
+		nodeShouldCenter = true
 		heldNode.set_deferred("freeze", false)
