af98ed121c81a590fa29ea39a6ac4d58a347d2a9
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-01 16:19:29 -0400

    feat: min and max values from effect slider

--- addons/resource_manager/effects_manager/effect_editor_panel.gd
+++ addons/resource_manager/effects_manager/effect_editor_panel.gd
@@ -4,7 +4,7 @@ extends Node
 # sends signal to ingredient_panel to close effect editor
 signal close_editor
 
-@onready var slider_container = $Content/ScrollContainer/EffectEditorsMarginContainer/EffectEditorsVBoxContainer
+@onready var SliderContainer = $Content/ScrollContainer/EffectEditorsMarginContainer/EffectEditorsVBoxContainer
 var ingredient:Ingredient
 
 ########################## SETUP ###########################
@@ -18,16 +18,16 @@ func _ready():
 	# Update label
 	$Content/Header/IngredientNameLabel.text = ingredient.name
 	# Load effect values
-	slider_container.get_node("HealingSlider")  .set_value(ingredient.effects.healing_poison)
-	slider_container.get_node("WarmSlider")     .set_value(ingredient.effects.warm_cold)
-	slider_container.get_node("CalmingSlider")  .set_value(ingredient.effects.calming_agitation)
-	slider_container.get_node("StrengthSlider") .set_value(ingredient.effects.strength_weakness)
-	slider_container.get_node("EnergySlider")   .set_value(ingredient.effects.energy_fatigue)
-	slider_container.get_node("LightSlider")    .set_value(ingredient.effects.light_dark)
-	slider_container.get_node("LuckSlider")     .set_value(ingredient.effects.luck_misfortune)
-	slider_container.get_node("ClaritySlider")  .set_value(ingredient.effects.clarity_confusion)
-	slider_container.get_node("DefenseSlider")  .set_value(ingredient.effects.defense_vulnerability)
-	slider_container.get_node("SwiftnessSlider").set_value(ingredient.effects.swiftness_slowness)
+	SliderContainer.get_node("HealingSlider")  .set_value(ingredient.effects.healing_poison)
+	SliderContainer.get_node("WarmSlider")     .set_value(ingredient.effects.warm_cold)
+	SliderContainer.get_node("CalmingSlider")  .set_value(ingredient.effects.calming_agitation)
+	SliderContainer.get_node("StrengthSlider") .set_value(ingredient.effects.strength_weakness)
+	SliderContainer.get_node("EnergySlider")   .set_value(ingredient.effects.energy_fatigue)
+	SliderContainer.get_node("LightSlider")    .set_value(ingredient.effects.light_dark)
+	SliderContainer.get_node("LuckSlider")     .set_value(ingredient.effects.luck_misfortune)
+	SliderContainer.get_node("ClaritySlider")  .set_value(ingredient.effects.clarity_confusion)
+	SliderContainer.get_node("DefenseSlider")  .set_value(ingredient.effects.defense_vulnerability)
+	SliderContainer.get_node("SwiftnessSlider").set_value(ingredient.effects.swiftness_slowness)
 
 ##################### EDITOR FUNCTIONS #####################
 func _on_back_button_pressed():
--- addons/resource_manager/effects_manager/effect_slider.gd
+++ addons/resource_manager/effects_manager/effect_slider.gd
@@ -1,29 +1,82 @@
 @tool
-extends Node
+extends Control
 
-# sends a signal to effect_editor_panel
-signal value_changed(effect_key:String, value:int)
+## Signals
+# sends a signal for min and max value changes (only min is used if show_range is false)
+signal min_changed(effect_key:String, value:int)
+signal max_changed(effect_key:String, value:int)
 
-@export var effect_key:String
-@onready var NumberSpinBox:SpinBox = $LabelHBox/ValueSpinBox
-@onready var NumberSlider:HSlider = $NumberSlider
+## Inspector vars
+@export var show_range:bool = false # show both min and max (if not we just use the min slider)
+@export var max:int = 30 # absolute value of the max number on the slider
+@export var effect_key:String # string of effect name
 
-## Update effect labels
+## Nodes
+@onready var MinGrabber = $SliderContainer/MinGrabber
+@onready var MinGrabberLabel = $SliderContainer/MinGrabber/MinLabel
+@onready var MaxGrabber = $SliderContainer/MaxGrabber
+@onready var MaxGrabberLabel = $SliderContainer/MaxGrabber/MaxLabel
+@onready var SliderRange = $SliderContainer/SliderRange
+
+var range_section_width:int
+var range_center:int
+
+## Track the current min and max values
+var min_value:int :
+	set(value):
+		min_value = value
+		MinGrabber.global_position.x = range_center + min_value * range_section_width
+		MinGrabberLabel.text = str(min_value)
+		min_changed.emit(effect_key, min_value)
+var max_value:int :
+	set(value):
+		max_value = value
+		MaxGrabber.global_position.x = range_center + max_value * range_section_width
+		MaxGrabberLabel.text = str(max_value)
+		max_changed.emit(effect_key, max_value)
+
+
 func _ready():
-	if not effect_key: return
-	$LabelHBox/PositiveLabel.text = EffectSet.positive_labels[effect_key]
-	$LabelHBox/NegativeLabel.text = EffectSet.negative_labels[effect_key]
+	# Update effect labels
+	if effect_key:
+		$LabelHBox/PositiveLabel.text = EffectSet.positive_labels[effect_key]
+		$LabelHBox/NegativeLabel.text = EffectSet.negative_labels[effect_key]
+	# Show max grabber and calculate offsets/section width
+	if not show_range: MaxGrabber.hide()
+	await get_tree().process_frame # wait for controls to get size
+	var slider_rect = SliderRange.get_rect()
+	range_section_width = round(slider_rect.size.x / (max * 2 + 1)) # double max for pos and neg, add 1 for zero
+	range_center = round(slider_rect.position.x + slider_rect.size.x / 2)
+	# Set default values
+	min_value = -max if show_range else 0
+	max_value = max
 
 
-## Keep slider and number in sync
-func _on_slider_changed(value:int):
-	NumberSpinBox.value = value
-func _on_number_changed(value:int):
-	NumberSlider.value = value
-	value_changed.emit(effect_key, value)
+func set_value(new_min:int, new_max:int = max):
+	min_value = new_min
+	max_value = new_max
+
+############################### DRAGGING SLIDERS ##############################
+var holding_min = false
+var holding_max = false
+
+func _on_min_grabber_input(event):
+	# check for click or release
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		holding_min = event.pressed
+	# move grabber and update value
+	if event is InputEventMouseMotion and holding_min:
+		var new_value = round((event.global_position.x - range_center) / range_section_width)
+		if abs(new_value) <= max and (not show_range or new_value <= max_value): # check valid
+			min_value = new_value
 
 
-## Set value of slider/number
-func set_value(value:int):
-	NumberSpinBox.value = value
-	NumberSlider.value = value
+func _on_max_grabber_input(event):
+	# check for click or release
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		holding_max = event.pressed
+	# move grabber and update value
+	if event is InputEventMouseMotion and holding_max:
+		var new_value = round((event.global_position.x - range_center) / range_section_width)
+		if abs(new_value) <= max and (not show_range or new_value >= min_value): # check valid
+			max_value = new_value
