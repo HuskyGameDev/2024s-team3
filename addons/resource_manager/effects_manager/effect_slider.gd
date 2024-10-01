197cae8bb2a834c42f061b01852d485e1968ddf5
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-01 16:41:46 -0400

    fix: don't reset effect values on reload

--- addons/resource_manager/effects_manager/effect_slider.gd
+++ addons/resource_manager/effects_manager/effect_slider.gd
@@ -27,13 +27,11 @@ var min_value:int :
 		min_value = value
 		MinGrabber.global_position.x = range_center + min_value * range_section_width
 		MinGrabberLabel.text = str(min_value)
-		min_changed.emit(effect_key, min_value)
 var max_value:int :
 	set(value):
 		max_value = value
 		MaxGrabber.global_position.x = range_center + max_value * range_section_width
 		MaxGrabberLabel.text = str(max_value)
-		max_changed.emit(effect_key, max_value)
 
 
 func _ready():
@@ -44,12 +42,16 @@ func _ready():
 	# Show max grabber and calculate offsets/section width
 	if not show_range: MaxGrabber.hide()
 	await get_tree().process_frame # wait for controls to get size
-	var slider_rect = SliderRange.get_rect()
-	range_section_width = round(slider_rect.size.x / (max * 2 + 1)) # double max for pos and neg, add 1 for zero
-	range_center = round(slider_rect.position.x + slider_rect.size.x / 2)
-	# Set default values
-	min_value = -max if show_range else 0
-	max_value = max
+	_on_resized()
+
+
+func _on_resized():
+	if not SliderRange: return
+	range_section_width = round(SliderRange.size.x / (max * 2 + 1)) # double max for pos and neg, add 1 for zero
+	range_center = round(SliderRange.global_position.x + SliderRange.size.x / 2)
+	# update grabber locations
+	min_value = min_value
+	max_value = max_value
 
 
 func set_value(new_min:int, new_max:int = max):
@@ -69,6 +71,7 @@ func _on_min_grabber_input(event):
 		var new_value = round((event.global_position.x - range_center) / range_section_width)
 		if abs(new_value) <= max and (not show_range or new_value <= max_value): # check valid
 			min_value = new_value
+			min_changed.emit(effect_key, min_value)
 
 
 func _on_max_grabber_input(event):
@@ -80,3 +83,4 @@ func _on_max_grabber_input(event):
 		var new_value = round((event.global_position.x - range_center) / range_section_width)
 		if abs(new_value) <= max and (not show_range or new_value >= min_value): # check valid
 			max_value = new_value
+			max_changed.emit(effect_key, max_value)
