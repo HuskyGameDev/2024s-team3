72e18725df067795e475da43a8e2c4f94248df73
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-01 17:44:17 -0400

    feat: add panel behind multi-effect editor

--- addons/resource_manager/loot_table_manager/table_row_inputs/min_and_max_effects/min_and_max_effects.gd
+++ addons/resource_manager/loot_table_manager/table_row_inputs/min_and_max_effects/min_and_max_effects.gd
@@ -3,16 +3,17 @@ extends Control
 
 const EffectRow = preload("res://addons/resource_manager/loot_table_manager/table_row_inputs/min_and_max_effects/min_and_max_effects_row.tscn")
 
-@onready var EffectOptions = $AddHBox/EffectOptions
+@onready var EffectOptions = $EffectContainer/AddHBox/EffectOptions
+@onready var EffectContainer = $EffectContainer
 
 func _on_add_button_pressed():
 	var selected_id = EffectOptions.selected
 	if selected_id < 0: return
 	## Add new effect editor row
 	var new_row = EffectRow.instantiate().with_data(EffectOptions.get_item_text(selected_id))
-	add_child(new_row)
-	move_child(new_row, get_child_count() - 2)
-	if get_child_count() >= 4: $AddHBox.hide()
+	EffectContainer.add_child(new_row)
+	EffectContainer.move_child(new_row, EffectContainer.get_child_count() - 2)
+	if EffectContainer.get_child_count() >= 4: $EffectContainer/AddHBox.hide()
 	## Remove option from dropdown
 	EffectOptions.remove_item(selected_id)
 	## Connect new row signals
@@ -21,6 +22,6 @@ func _on_add_button_pressed():
 
 func _on_row_deleted(deleted_row_key:String):
 	# need to use <= because when the signal is emitted the node hasn't been removed from the tree yet
-	if get_child_count() <= 4: $AddHBox.show() 
+	if EffectContainer.get_child_count() <= 4: $EffectContainer/AddHBox.show() 
 	## Add option back to dropdown
 	EffectOptions.add_item(deleted_row_key)
