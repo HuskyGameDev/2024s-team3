476b81a8cf337809a9d1297c5807734be055d86a
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-07 15:31:03 -0400

    feat: create station that switches which item is in the scene

---
+++ screens/main/stations/station.gd
@@ -0,0 +1,26 @@
+extends Node
+
+signal change_ingredient(ingredient:Ingredient, position:Vector2)
+
+@onready var center_of_station = self.position - Vector2(0, 80)
+
+func _on_body_enter_mortar(body):
+	if not "data" in body: return
+	if not body.data is Ingredient: return
+	
+	## Animate object movement to center of station
+	body.gravity_scale = 0
+	var ingredient_position = body.global_position
+	var time_to_animate = (center_of_station.distance_to(ingredient_position)) / 1000
+	var tween = create_tween().bind_node(body)
+	tween.tween_property(body, "global_position", center_of_station, time_to_animate).from(ingredient_position)
+	await tween.finished
+	
+	## Delete old ingredient and spawn new one
+	#TODO get ingredient
+	body.queue_free()
+	var new_ingredient = load(ResourcePaths.get_ingredient_path("nightshade_petals"))
+	$ValidArea/CollisionShape2D.disabled = true
+	change_ingredient.emit(new_ingredient, center_of_station)
+	await get_tree().create_timer(0.1).timeout # wait for item to be thrown of collision area
+	$ValidArea/CollisionShape2D.disabled = false
