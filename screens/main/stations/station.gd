4279c82641c894c62ffcf3afc9daf9977521084e
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-08 12:47:19 -0400

    feat: spawn correct item from stations

--- screens/main/stations/station.gd
+++ screens/main/stations/station.gd
@@ -20,9 +20,9 @@ func _on_body_enter_mortar(body):
 	await tween.finished
 	
 	## Delete old ingredient and spawn new one
-	#TODO get ingredient
 	body.queue_free()
-	var new_ingredient = load(ResourcePaths.get_ingredient_path("nightshade_petals"))
+	var new_ingredient_id = "%s_%s" % [Ingredient.action_to_string(action), body.data.id]
+	var new_ingredient = load(ResourcePaths.get_ingredient_path(new_ingredient_id))
 	$ValidArea/CollisionShape2D.disabled = true
 	change_ingredient.emit(new_ingredient, center_of_station)
 	await get_tree().create_timer(0.1).timeout # wait for item to be thrown of collision area
