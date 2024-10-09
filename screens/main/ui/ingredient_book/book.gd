56755547bf6df37506a13d78810a732430d88484
Ben Kornas <93051981+Beancan7@users.noreply.github.com>
2024-10-09 14:52:32 -0400

    Added working book UI, Added chain to bell, Added Sprites
    The ingredient book UI has been implemented although it is a little rough. I also added the chain to the bell button so it wouldn't just be a floating bell. Finally I added all the sprites for our 10 ingredients.

--- screens/main/ui/ingredient_book/book.gd
+++ screens/main/ui/ingredient_book/book.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var forward = $Forward
 @onready var back = $Back
 @onready var anim = $"Book Animation"
+@onready var values = $Values
 
 
 var batWing = preload("res://common/items/ingredients/bat_wing/bat_wing.tres")
@@ -17,8 +18,10 @@ var pineResin = preload("res://common/items/ingredients/pine_resin/pine_resin.tr
 var poisonIvy = preload("res://common/items/ingredients/poison_ivy/poison_ivy.tres")
 var sunFlower = preload("res://common/items/ingredients/sunflower_seeds/sunflower_seeds.tres")
 var thistleRoot = preload("res://common/items/ingredients/thistle_root/thistle_root.tres")
+var eyeOfNewt = preload("res://common/items/ingredients/eye_of_a_newt/eye_of_a_newt.tres")
 
 
+var up: bool = false
 
 var current_ingr_key
 @export var max_ingr = 10
@@ -28,16 +31,20 @@ func _ready():
 	current_ingr_key = 0
 	_update_data()
 
+func _move_up():
+	if (!up):
+		position = Vector2(306, 858)
+		up = true
+	else:
+		position = Vector2(306, 1248)
+		up = false
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _add_one():
 	current_ingr_key += 1
 	_update_data()
-	
 
 
 func _sub_one():
@@ -45,7 +52,7 @@ func _sub_one():
 	_update_data()
 
 func _update_data():
-	var current_ingr
+	var current_ingr: Ingredient
 	match current_ingr_key:
 		0:
 			current_ingr = batWing
@@ -66,13 +73,28 @@ func _update_data():
 		8:
 			current_ingr = thistleRoot
 		9:
-			pass
+			current_ingr = eyeOfNewt
 	
 	
 	# Change all the data
-	print("we made it")
-	print(current_ingr.description)
 	description.text = current_ingr.description
+	ingredient.texture = current_ingr.image
+	var valuesArray = values.get_children()
+	
+	valuesArray[0].get_node("Value").text = str(current_ingr.effects.healing_poison) 
+	valuesArray[1].get_node("Value").text = str(current_ingr.effects.warm_cold)
+	valuesArray[2].get_node("Value").text = str(current_ingr.effects.calming_agitation)
+	valuesArray[3].get_node("Value").text = str(current_ingr.effects.strength_weakness)
+	valuesArray[4].get_node("Value").text = str(current_ingr.effects.energy_fatigue)
+	valuesArray[5].get_node("Value").text = str(current_ingr.effects.light_dark)
+	valuesArray[6].get_node("Value").text = str(current_ingr.effects.luck_misfortune)
+	valuesArray[7].get_node("Value").text = str(current_ingr.effects.clarity_confusion)
+	valuesArray[8].get_node("Value").text = str(current_ingr.effects.defense_vulnerability)
+	valuesArray[9].get_node("Value").text = str(current_ingr.effects.swiftness_slowness)
+	
+	#for i in valuesArray.size():
+		#valuesArray[i].value 
+	
 	# yada yada
 	
 	
@@ -86,7 +108,7 @@ func _update_data():
 
 
 func _on_forward_pressed() -> void:
-	if (current_ingr_key < max_ingr):
+	if (current_ingr_key < (max_ingr - 1)):
 		anim.play("Forward")
 	else:
 		pass
